bbfeafbbc202565c71e80302397f2e2be04c0f3b
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-20 14:47:10 -0700

    added 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,20 +12,23 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			product = i * j;
-			if (product < 10)
+			if (j == 0)
 			{
 				_putchar('0' + product);
 			}
-			else
-			{
-				_putchar('0' + (product / 10));
-				_putchar('0' + (product % 10));
-			}
-			if (j != 9)
+			else if (product < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
 			}
 		}
 		_putchar('\n');
