cda36f4339a597affeaef61eac2c04c6d7288e98
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-20 18:21:37 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -41,7 +41,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(((p / 100) % 10) + '0');
 					_putchar((p / 10) + '0');
-					_putchar((p % 10)) + '0';
+					_putchar((p % 10) + '0');
 				}
 			}
 			_putchar('\n');
