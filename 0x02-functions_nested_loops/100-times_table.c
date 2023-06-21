58df9db4371ac707edffefcedaf13b9cbdb000ac
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-21 11:57:41 -0700

    added void print_times_table(int n)

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,33 +12,23 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
+			_putchar('0');
 			for (j = 0; j <= n; j++)
 			{
 				p = i * j;
-				if (j == 0)
+				_putchar(',');
+				_putchar(' ');
+				if (p <= 99)
+					_putchar(' ');
+				if (p < 10)
+					_putchar(' ');
+				if (p >= 10 && p <= 99)
 				{
-					_putchar(p + '0');
-				}
-				else if (j != 0 && p < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(p + '0');
-				}
-				else if (p >= 10 && p < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
 					_putchar((p / 10) + '0');
-					_putchar((p % 10) + '0');
+					_putchar(p + '0');
 				}
-				else if (p >= 100)
+				if (p >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar(((p / 100) % 10) + '0');
 					_putchar((p / 10) + '0');
 					_putchar((p % 10) + '0');
