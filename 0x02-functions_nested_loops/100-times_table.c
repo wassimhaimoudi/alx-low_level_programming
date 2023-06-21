d158ee4ad93d6ee01814aa95af22a8e3ae41e0c2
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-21 12:22:40 -0700

    added task100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,19 +20,18 @@ void print_times_table(int n)
 				_putchar(' ');
 				if (p <= 99)
 					_putchar(' ');
-				if (p < 10)
+				if (p <= 9)
 					_putchar(' ');
-				if (p >= 10 && p <= 99)
-				{
-					_putchar((p / 10) + '0');
-					_putchar(p + '0');
-				}
 				if (p >= 100)
 				{
-					_putchar(((p / 100) % 10) + '0');
-					_putchar((p / 10) + '0');
-					_putchar((p % 10) + '0');
+					_putchar((p / 100) + '0');
+					_putchar((p  / 10) % 10 + '0');
 				}
+				else if (p >= 10 && p <= 99)
+				{
+					_putchar((p / 10) + '0');
+				}
+				_putchar((p % 10) + '0');
 			}
 			_putchar('\n');
 		}
