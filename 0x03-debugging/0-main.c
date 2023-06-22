37504a3b2e6d8d6e6b4f1e222bd8c701763e2ac4
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-22 10:59:08 -0700

    added 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - tests the function positive_or_negative for the value of 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
