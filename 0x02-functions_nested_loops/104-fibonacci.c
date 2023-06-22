127d2664b56edd645ee3380c8a10fe60728cd967
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-21 17:47:00 -0700

    added task104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,18 +17,32 @@ int main(void)
  */
 void fibonacci_98(void)
 {
-	unsigned int t1, t2, n;
+	unsigned int t1, t2, n1, n2, r1, r2;
 	int i;
 
 	t1 = 1;
 	t2 = 2;
-	printf("%u, %u", t1, t2);
-	for (i = 3; i <= 100; i++)
+	printf("%u", t2);
+	for (i = 3; i < 91; i++)
 	{
-		n = t1 + t2;
-		printf(", %u", n);
-		t1 = t2;
-		t2 = n;
+		printf(", %u", t2);
+		t2 = t2 + t1;
+		t1 = t2 - t1;
+	}
+
+	n1 = t1 / 1000000000;
+	n2 = t1 % 1000000000;
+	r1 = t2 / 1000000000;
+	r2 = t2 % 1000000000;
+
+	for (i = 92; i < 99; i++)
+	{
+		printf(", %u", r1 + (r2 / 1000000000));
+		printf("%u", r2 % 1000000000);
+		r1 = r1 + n1;
+		n1 = r1 - n1;
+		r2 = r2 + n2;
+		n2 = r2 - n2;
 	}
 	printf("\n");
 }
