4815995f8bdf0581cdbe74ba4e073b59616cf5f3
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-23 16:54:35 -0700

    added task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,11 +16,11 @@ void fizz_buzz(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("Fizz ");
+			printf("Fizz Buzz ");
 		else if (i % 3 == 0)
-			printf("Buzz ");
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("Buzz ");
 		else
 			printf("%d ", i);
 	}
