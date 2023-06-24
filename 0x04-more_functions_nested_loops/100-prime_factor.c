38ab193e01a3ffd40b19113335133a9bdf7c10f3
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-23 19:37:11 -0700

    added 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prime_factor text.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	long int n, div, maxFact;
+
+	n = 612852475143;
+	div = 2;
+
+	while (n != 0)
+	{
+		if (n % div != 0)
+		{
+			div++;
+		}
+		else
+		{
+			maxFact = n;
+			n = n / div;
+			if (n == 1)
+			{
+				printf("%ld", maxFact);
+			}
+		}
+	}
+	return (0);
+
+
+}
