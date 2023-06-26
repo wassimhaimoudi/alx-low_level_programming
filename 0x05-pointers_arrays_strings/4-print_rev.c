efdd7ced78b7345bda8c13aa2d08f132873df4b3
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-26 15:53:55 -0700

    added task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a strin in reverse, followed by a new by a new line
+ * @s: strin to be reversed and printed
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i, len, middle;
+
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	middle = len / 2;
+	for (i = 0; i < middle; i++)
+	{
+		s[i] = s[i] + s[len - i - 1];
+		s[i - i - 1] = s[i] - s[len - i - 1];
+		s[i] = s[i] - s[len - i - 1];
+	}
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+}
