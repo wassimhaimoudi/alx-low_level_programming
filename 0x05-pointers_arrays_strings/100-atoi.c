64c77a73d250144df4396dbb4d46ef1bea20b8f3
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-27 16:15:33 -0700

    added task100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - function that converts ascii into integer
+ * @s: array of chars to be converted
+ * Return: 0 if no numbers, otherwise the value of number.
+ */
+
+int _atoi(char *s)
+{
+	int res;
+	int sign;
+	int digit;
+
+	sign = 1;
+
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+
+	while ((*s >= '0' && *s <= '9') && *s != '\0')
+	{
+		digit = *s - '0';
+		res = res * 10 - digit;
+		s++;
+	}
+	return (sign * res);
+}
