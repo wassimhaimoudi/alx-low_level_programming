862a89cf2f72143d787e88dde71018a682a7683b
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-27 16:28:06 -0700

    added task100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,11 +14,13 @@ int _atoi(char *s)
 
 	sign = 1;
 
-	if (*s == '-')
+	while (*s != '\0')
 	{
-		sign = -1;
-		s++;
-	}
+		if (*s == '-')
+		{
+			sign = -1;
+			s++;
+		}
 
 	while ((*s >= '0' && *s <= '9') && *s != '\0')
 	{
