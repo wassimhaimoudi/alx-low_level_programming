39dc5d080bd70d2dd1c28108cfb87fbf62a4fbe2
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-29 12:36:04 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,15 +10,15 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, size_src;
+	int i, j, size_src, n2 = n;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	;
 	for (size_src = 0; src[size_src] != '\0'; size_src++)
 	;
 	if (n > size_src)
-		n = size_src;
-	for (j = 0; j <= n; j++)
+		n2 = size_src;
+	for (j = 0; j < n2; j++)
 	{
 		dest[i] = src[j];
 		i++;
