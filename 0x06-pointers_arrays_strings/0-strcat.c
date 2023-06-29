cc8dc2fad1b7a4f2e122330805656c7e4782d626
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-29 08:55:22 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,10 +9,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	while (*dest != '\0')
-		dest++;
-	while (*src != '\0')
-		*dest++ = *src++;
-	*dest = '\0';
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
 	return (dest);
 }
