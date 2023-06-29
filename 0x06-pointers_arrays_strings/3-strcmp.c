744c8294a657b3ed2bf721704f70af91657a8f4b
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-29 16:38:57 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,15 +13,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int value;
 
-	while (*s1 == *s2 && *s1 != '\0')
+	while (*s1 == *s2)
 	{
+		if (*s1 != '\0')
+		{
+			return (0);
+		}
 		s1++;
 		s2++;
 	}
-
-	if (s1 != s2)
-		value = *s1 - *s2;
-
+	value = *s1 - *s2;
 	return (value);
-
 }
