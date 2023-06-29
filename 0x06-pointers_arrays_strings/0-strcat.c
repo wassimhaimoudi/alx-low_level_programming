f284ae0b357c4d10cc6fb2086ac615b9092b3c67
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-29 08:45:41 -0700

    added alt task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,10 +9,18 @@
 
 void _strcat(char *dest, char *src)
 {
-	while (*dest != '\0')
-		dest++;
-	while (*src != '\0')
-		*dest++ = *src++;
-	*dest = '\0';
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
