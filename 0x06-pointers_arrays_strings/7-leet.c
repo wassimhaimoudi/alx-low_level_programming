7d770cce16911f82d9be45e66ffe87e3e923074c
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-30 09:58:09 -0700

    ta7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,18 +9,16 @@
 char *leet(char *s)
 {
 	int i, j;
-	char l[] = "AEOTL";
-	char n[] = "43071";
+	char l[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 7; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (s[i] == l[j] || s[i] == l[j] + 32)
+			if (s[i] == l[j])
 				s[i] = n[j];
-
 		}
-
 	}
 	return (s);
 }
