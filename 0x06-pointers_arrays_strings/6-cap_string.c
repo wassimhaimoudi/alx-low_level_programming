78d08fad648f6d936d2dafab4f7f09bd15d4af41
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-06-29 17:41:03 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,7 @@ char *cap_string(char *s)
 	int i;
 
 	for (i = 1; s[i] != '\0'; i++)
+	{
 		if (str[i - 1] == ' ' ||
 			str[i - 1] == '\t' ||
 			str[i - 1] == '\n' ||
@@ -26,9 +27,10 @@ char *cap_string(char *s)
 			str[i - 1] == '}')
 		{
 			if (s[i] >= 'a' && s[i] <= 'z')
-				s -= ('a' - 'A');
+				s = s - ('a' - 'A');
 		}
+	}
 	if (s[0] >= 'a' && s[0] <= 'z')
-		s -= ('a' - 'A');
+		s = s - ('a' - 'A');
 	return (s);
 }
