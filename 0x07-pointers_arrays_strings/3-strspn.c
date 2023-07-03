17074dfef1b6035605efddc957ea512d17d1b5fc
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-07-03 15:33:05 -0700

    task3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		bool found_match = false;
 
-		for (j = 0; accept[j] = '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
 			{
