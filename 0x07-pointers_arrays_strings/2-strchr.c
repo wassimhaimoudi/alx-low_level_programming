fc21cf9d0b229ea2a54030c2ea9c4c6d007b600c
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-07-03 13:57:48 -0700

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,15 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	while (*s != '\0')
 	{
 		if (*s == c)
+		{
 			return (s);
+		}
 		s++;
 	}
+	if (c == '\0')
+		return (s);
 	return (NULL);
 }
