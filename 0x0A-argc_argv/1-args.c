62fba5ee0d335a900969211940205fd22ce2bd67
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-07-07 15:36:42 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ * Description: a program that prints the number of arguments passed into it.
+ * 		followed by a new line.
+ * @argc: the number of args.
+ * @argv: 2d array of args.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	print("%d\n", argc - 1);
+	return (0);
+}
