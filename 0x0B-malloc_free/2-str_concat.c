df4eacc1f8de2c189f11d3ad1f522d1be4343e24
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-07-11 13:27:01 -0700

    task02 done

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: seconf string.
+ * Return: pointer to a newly allocated space in memory which contains
+ * the contents of s1 followd by the contents of s2 and null terminated.
+ * Null on failure.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, j, len1, len2;
+	char *p;
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+	;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	p = malloc(sizeof(char) * (len1 + len2 + 2));
+
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (j = 0 ; j < len1 + len2; j++)
+	{
+		p[len1 + j] = s2[j];
+	}
+	p[len1 + len2] = '\0';
+	return (p);
+}
