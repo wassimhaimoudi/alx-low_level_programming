39e96b44ac03f6064f4f4b8eda33762736b56a0a
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-07-13 09:55:00 -0700

    task 01 done fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,8 +37,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len1; i++)
 		p[i] = s1[i];
 
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; n < len2 && i < len1 + n; j++, i++)
 		p[i] = s2[j];
+
+	for (j = 0; n >= len2 && i < len1 + len2; i++, j++)
+		p[i] = s2[j];
+
 	p[len1 + n] = '\0';
 
 	return (p);
