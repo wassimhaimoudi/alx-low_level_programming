90569fd17c7c41437562d67349f6ebaa8813deb9
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-07-21 19:31:39 -0700

    task03 done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _HEADER_
 #define _HEADER_
+
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -7,4 +8,5 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif
