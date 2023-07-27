adc66e8017a28c8e85839d17f474bf11e8c2d96d
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-07-27 08:07:10 -0700

    task00 done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,22 +8,18 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count; 
+	size_t count;
 
 	count = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		
 			printf("[0] (nil)\n");
-		
 		else
-		
 			printf("[%d] %s\n", h->len, h->str);
 
 		count++;
 		h = h->next;
-		
 	}
 	return (count);
 }
