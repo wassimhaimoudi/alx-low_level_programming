7ecf14abe22b26435d66256e43c23e821a34de7b
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-07-27 17:08:48 -0700

    task 02 fixed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,6 +14,8 @@ list_t *add_node(list_t **head, const char *str)
 	if (head == NULL)
 		return (NULL);
 	new = (list_t *)malloc(sizeof(list_t));
+	if (!new)
+		return(NULL);
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = *head;
