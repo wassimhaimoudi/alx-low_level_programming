6ef012b102e679e524abccf13cab268faa8a779d
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-08-01 15:00:42 -0700

    task06 done

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the hadnode of a linked list.
+ * @head: the head of the linked list
+ * Return: 0 if lst is empty, otherwise the data of the head node.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *current;
+	int data;
+
+	if (head == NULL)
+		return(-1);
+
+	data = (*head)->n;
+	current = *head;
+	*head = (*head)->next;
+
+	free(current);
+
+	return (data);
+
+}
