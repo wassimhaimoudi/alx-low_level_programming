29222dad7203c8e8f46edf5b54efa7e270606442
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-08-01 08:45:01 -0700

    task02 fixed

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,13 +7,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-        size_t count;
+	size_t count;
 
-        count = 0;
-        while (h)
-        {
-                h = h->next;
-                count++;
-        }
-        return (count);
+	count = 0;
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
