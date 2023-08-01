1eee3c0c25003e1a4fafd6eac10a8e326c67bf9c
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-08-01 10:09:02 -0700

    task02 fixed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,6 +12,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new;
 
 	new = (listint_t *)malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 	new->n = n;
 	new->next = *head;
 	*head = new;
