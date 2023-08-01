c51d199a002e880b349a4bb62a89c2d645773332
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-08-01 08:42:20 -0700

    task02 done

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints all linked list elements
+ * @h: head of linked list
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+        size_t count;
+
+        count = 0;
+        while (h)
+        {
+                h = h->next;
+                count++;
+        }
+        return (count);
+}
