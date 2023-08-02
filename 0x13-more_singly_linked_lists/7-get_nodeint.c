b1d6cb22932d23e20bda8c5330bcc9250547e979
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-08-02 15:27:33 -0700

    task07 fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current;
 
 	current = head;
-	for (i = 0; i < index; i++)
+	for (i = 0; current && i < index; i++)
 	{
 		current = current->next;
 	}
