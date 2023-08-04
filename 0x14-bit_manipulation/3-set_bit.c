963f92e590c568e483a0d0331f1dff776b5475f6
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-08-04 15:17:29 -0700

    task03 done

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * set_bit- function name
+ * Description: sets thre value of a bit to 1 at a given index
+ *
+ * @n: number passed to the function
+ * @index: index of the bit to set.
+ * Return: 1 if it worked, -1 if an error occured
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int max;
+	unsigned long int mask;
+
+	max = sizeof(unsigned long int) * 8 - 1;
+	if (index > max)
+		return (-1);
+
+	mask = 1 << index;
+	*n = *n | mask;
+	return (*n);
+}
