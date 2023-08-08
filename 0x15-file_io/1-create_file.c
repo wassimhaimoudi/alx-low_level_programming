5bd84431f989db54ac46309847644e59b601d9b8
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-08-07 17:24:30 -0700

    task01 fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,20 +19,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i]; i++)
 		;
-
-		file_d = open(filename, O_CREAT | O_RDWR, 0600);
-		number_w = write(file_d, text_content, i);
-
-		if (file_d == -1 || number_w == -1)
-			return (-1);
 	}
-	else
-	{
-		file_d = open(filename, O_CREAT, 0600);
-		if (file_d == -1)
-			return (-1);
-	}
+	file_d = open(filename, O_CREAT | O_RDWR, 0600);
+	number_w = write(file_d, text_content, i);
 
+	if (file_d == -1 || number_w == -1)
+		return (-1);
+	
 	close(file_d);
 
 	return (1);
