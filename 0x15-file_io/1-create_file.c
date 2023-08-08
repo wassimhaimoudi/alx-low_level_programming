e9b407826497b6259b08f5482f972ac01b8cb563
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-08-08 10:53:46 -0700

    task01 fix1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		file_d = open(filename, O_CREAT, 0600);
+		file_d = open(filename, O_CREAT | O_TRUNC, 0600);
 		if (file_d == -1)
 			return (-1);
 	}
