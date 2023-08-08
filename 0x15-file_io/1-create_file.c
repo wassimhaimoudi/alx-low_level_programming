0bf7bf10cbe15f00c1957f0d20f2b8fedf9160c7
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-08-07 17:14:11 -0700

    task01 done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - function name
+ * Description: function that creates a file.
+ *
+ * @filename: the file tio be created
+ * @text_content: text to write into the file
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, file_d, number_w;
+
+	if (!filename)
+		return (-1);
+	if (text_content)
+	{
+		for (i = 0; text_content[i]; i++)
+		;
+
+		file_d = open(filename, O_CREAT | O_RDWR, 0600);
+		number_w = write(file_d, text_content, i);
+
+		if (file_d == -1 || number_w == -1)
+			return (-1);
+	}
+	else
+	{
+		file_d = open(filename, O_CREAT, 0600);
+	}
+
+	close(file_d);
+
+	return (1);
+}
