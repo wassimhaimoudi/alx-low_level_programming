86c9fb0abd800526d4021448c3672a03bbb6b2ab
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-08-24 04:02:32 -0700

    task100

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,5 +26,19 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void _print_first(void);
+
+/**
+ *struct listNodeFP - node containing function pointers
+ *@_callFirst - function pointer
+ * @next: points to the next function
+ *Description: struct type for function and functionpointers
+ */
+struct listNodeFP
+{
+        void(* _callFirst)(void);
+        struct listNodeFP* next;
+};
+void executeList(struct listNodeFP* head);
 
 #endif
