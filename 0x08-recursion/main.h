8cc41d047c7f316c7acc1f0be86327786e38b1af
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-09-20 18:55:52 -0700

    task 100 & 101

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(int c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -6,3 +9,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+#endif
